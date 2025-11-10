1db64b8a9293cbfc4e26ea1bc7e94b104c3b3bfa
Fernando Barrera <barrerafernando54@gmail.com>
2025-11-10 07:22:31 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,27 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import WhatsAppButton from "./components/WhatsAppButton";
 import BackToTop from "./components/BackToTop";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 export default function Home() {
   return (
-    <div className="pt-16 scroll-smooth">
-      <Navbar />
-      <About />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-      <WhatsAppButton />
-      <BackToTop />
+    <div className="relative pt-16 scroll-smooth">
+      {/* Particles Background - Cubre toda la página */}
+      <div className="fixed inset-0 z-0">
+        <ParticlesBackground />
+      </div>
+      
+      {/* Contenido principal - Por encima de las partículas */}
+      <div className="relative z-10">
+        <Navbar />
+        <About />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+        <WhatsAppButton />
+        <BackToTop />
+      </div>
     </div>);
 }
