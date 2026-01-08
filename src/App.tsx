7ecdf9146f2712087ce37338babfd357b79674ec
Fernando Barrera <barrerafernando54@gmail.com>
2026-01-07 20:43:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import About from './components/About'
+import Navbar from './components/Navbar'
+import Experience from './components/Experience'
 
 function App() {
 
   return (
     <div>
+      <Experience />
+      <Navbar />
       <About />
     </div>
   )
