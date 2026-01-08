fd32c8c66d70f71a96dc5a046bf32d04fe12b0f0
Fernando Barrera <barrerafernando54@gmail.com>
2026-01-07 20:50:51 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Skills from './components/Skrills'
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
       <Navbar />
       <About />
       <Experience />
+      <Projects />
+      <Skills />
     </div>
   )
 }
